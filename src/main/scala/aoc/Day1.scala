1fb3159a03d116b5f3894bd3b3251fb52dab9ed5
blerou <sulik.szabolcs@gmail.com>
2019-12-01 21:48:12 +0100

    day1 - part2

--- src/main/scala/aoc/Day1.scala
+++ src/main/scala/aoc/Day1.scala
@@ -30,17 +30,59 @@ What is the sum of the fuel requirements for all of the modules on your spacecra
 
   def fuelRequired(mass: Int): Int = math.floor(mass / 3).toInt - 2
 
-  def part1(): Unit = {
+  def moduleMass(): Iterator[Int] = {
     val inputPath = "day1/input"
     import scala.io.Source
     val modules = Source.fromResource(inputPath).getLines
-    val modulesMass = modules map Integer.parseInt
+    modules map Integer.parseInt
+  }
+
+  def part1(): Unit = {
+    val modulesMass = moduleMass()
     val moduleFuel = modulesMass map fuelRequired
     val fuel = moduleFuel.sum
     println(s"part 1 solution: $fuel")
   }
 
+  /*
+
+--- Part Two ---
+
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+
+Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+
+So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+    A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+    At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+    The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
+
+   */
+
+  def fuelFuel(mass: Int): Int = fuelFuel(mass, 0)
+
+  @scala.annotation.tailrec
+  def fuelFuel(mass: Int, all: Int): Int = {
+    val fuel = fuelRequired(mass)
+    if (fuel <= 0) {
+      all
+    } else {
+      fuelFuel(fuel, all + fuel)
+    }
+  }
+
+  def part2(): Unit = {
+    val modulesMass = moduleMass()
+    val moduleFuel = modulesMass map fuelFuel
+    val fuel = moduleFuel.sum
+    println(s"part 2 solution: $fuel")
+  }
+
   def main(args: Array[String]): Unit = {
     part1()
+    part2()
   }
 }
--- src/test/scala/aoc/Day1Test.scala
+++ src/test/scala/aoc/Day1Test.scala
@@ -1,13 +1,20 @@
 package aoc
 
-import Day1.fuelRequired
 import org.scalatest.flatspec.AnyFlatSpec
 
 class Day1Test extends AnyFlatSpec{
+  import Day1._
+
   "The mass" should "match the calculated value" in {
     assert(2 === fuelRequired(12))
     assert(2 === fuelRequired(14))
     assert(654 === fuelRequired(1969))
     assert(33583 === fuelRequired(100756))
+  }
+
+  "The fuel fuel meta" should "match the examples" in {
+    assert(2 === fuelFuel(14))
+    assert(966 === fuelFuel(1969))
+    assert(50346 === fuelFuel(100756))
   }
 }
