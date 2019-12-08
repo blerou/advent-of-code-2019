918504350f40d0623cf4ff061ff415223b6827a7
blerou <sulik.szabolcs@gmail.com>
2019-12-08 20:36:52 +0100

    day6 - part2
    the problem space is so small that you barely need
    any sofistication (`seen`)

--- src/test/scala/aoc/Day6Test.scala
+++ src/test/scala/aoc/Day6Test.scala
@@ -4,10 +4,18 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class Day6Test extends AnyFlatSpec {
   import Day6._
+
   "The example" should "match" in {
     val rawInput = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"
     val input = parse(rawInput.split('\n').toIterable)
     val result = orbitCount(input)
     assert(result === 42)
+  }
+
+  "The YOU-SAN example" should "work" in {
+    val rawInput = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"
+    val input = parse(rawInput.split('\n').toIterable)
+    val result = orbitalTransfers(input)
+    assert(result === 4)
   }
 }
